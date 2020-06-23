e1909644ff95d14d530a53a09d6d52b5dca83068
Michel Maia <dimermichel@gmail.com>
2020-06-23 03:30:21 -0400

    Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.json({ message: 'Hello GoStack.' }));
+
+app.listen(3333, () => {
+  console.log('Server running on port 3333. 🏍 💨');
+});
