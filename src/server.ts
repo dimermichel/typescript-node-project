73423cad39bfaf93a651756f632ae57d4fc5e66c
Michel Maia <dimermichel@gmail.com>
2020-07-15 22:47:54 -0400

    Add CORS Support

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -11,6 +12,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
